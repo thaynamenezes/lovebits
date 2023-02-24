0289d681bf696f87257f3da6463d65d4fbb3fe9e
Thayna <thaynamenezes01@gmail.com>
2023-02-24 00:43:46 -0500

    Changed button_callback function and started to add generic parameters

--- play_button.py
+++ play_button.py
@@ -2,17 +2,21 @@ import RPi.GPIO as GPIO
 import vlc
 import time
 
+##
+# FILE IS A WORK IN PROGRESS ***HARD CODED***
+##
+
 # Set the GPIO mode to BCM
 GPIO.setmode(GPIO.BCM)
 
 # Define the GPIO pin that the button is connected to
-button_pin = 26
+button1 = 26
 
 # Set up the button pin as an input with a pull-up resistor
-GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Define a callback function to run when the button is pressed
-def button_callback(channel):
+def button_callback(file_name):
     # Print to console that button has been pressed
     print("GPIO 26 pressed")
     
@@ -20,7 +24,7 @@ def button_callback(channel):
     media_player = vlc.MediaPlayer()
     
     # media object
-    media = vlc.Media("{file_name}"+".avi")
+    media = vlc.Media(file_name)
     
     # setting media to the media player
     media_player.set_media(media)
@@ -34,9 +38,7 @@ def button_callback(channel):
     # release object, closing vlc
     media_player.release()
     
-# Add the button callback function to the button pin
-GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=button_callback)
-
-# Keep the script running
-while True:
-    pass
+# Add the button callback function to the button pin 
+#STILL WORKING ON MAKING THIS A GENERIC FUNCTION
+#Used lambda (anonymous function) so we can use parameters in the function call
+GPIO.add_event_detect(button1, GPIO.FALLING, callback=lambda x: button_callback("insert file name here"))
